bb03b637f3d179168654bcfb409c481a267f68d9
Chris Fallin <cfallin@c1f.net>
2016-05-17 15:23:20 -0400

    Update docs for 2d8fc62632b219451486a722de607438e3b9caaf

--- implementors/core/ops/trait.DerefMut.js
+++ implementors/core/ops/trait.DerefMut.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["libc"] = [];
+implementors["libc"] = [];implementors["smallvec"] = ["impl&lt;A: <a class='trait' href='smallvec/trait.Array.html' title='smallvec::Array'>Array</a>&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.DerefMut.html' title='core::ops::DerefMut'>DerefMut</a> for <a class='struct' href='smallvec/struct.SmallVec.html' title='smallvec::SmallVec'>SmallVec</a>&lt;A&gt;",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
---
+++ implementors/core/ops/trait.Drop.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["smallvec"] = ["impl&lt;'a, T: 'a&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.Drop.html' title='core::ops::Drop'>Drop</a> for <a class='struct' href='smallvec/struct.SmallVecMoveIterator.html' title='smallvec::SmallVecMoveIterator'>SmallVecMoveIterator</a>&lt;'a, T&gt;","impl&lt;A: <a class='trait' href='smallvec/trait.Array.html' title='smallvec::Array'>Array</a>&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.Drop.html' title='core::ops::Drop'>Drop</a> for <a class='struct' href='smallvec/struct.SmallVec.html' title='smallvec::SmallVec'>SmallVec</a>&lt;A&gt;",];implementors["itertools"] = ["impl&lt;'a, K, I, F&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.Drop.html' title='core::ops::Drop'>Drop</a> for <a class='struct' href='itertools/struct.Group.html' title='itertools::Group'>Group</a>&lt;'a, K, I, F&gt; <span class='where'>where I: <a class='trait' href='https://doc.rust-lang.org/nightly/core/iter/iterator/trait.Iterator.html' title='core::iter::iterator::Iterator'>Iterator</a>, I::Item: 'a</span>","impl&lt;'a, I&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.Drop.html' title='core::ops::Drop'>Drop</a> for <a class='struct' href='itertools/struct.Chunk.html' title='itertools::Chunk'>Chunk</a>&lt;'a, I&gt; <span class='where'>where I: <a class='trait' href='https://doc.rust-lang.org/nightly/core/iter/iterator/trait.Iterator.html' title='core::iter::iterator::Iterator'>Iterator</a>, I::Item: 'a</span>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
